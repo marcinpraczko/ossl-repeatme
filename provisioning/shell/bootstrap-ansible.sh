0f61edf7e5d367f8412b72ad9abc70b48c7d0fd1
Marcin Praczko <marcin.praczko@gmail.com>
2013-11-28 22:15:36 +0000

    Add headers releted with GPL.
    * Add GPL headers to files.

--- provisioning/shell/bootstrap-ansible.sh
+++ provisioning/shell/bootstrap-ansible.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 # ----------------------------------------------------------------------------
-# 2013.09 by Marcin Praczko
+# (C) 2013, Marcin Praczko, <marcin.praczko@gmail.com>
 #
-# OSSL-repeatme project
+# This file is part of OSSL-repeatme
+#
+# OSSL-repeatme is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OSSL-repeatme is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OSSL-repeatme. If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------
